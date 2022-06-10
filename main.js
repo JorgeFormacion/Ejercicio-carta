b160a8f772579fba6599589e6548a88ef877d57f
Jorge Ramirez <jorgeramirezinstituto@gmail.com>
2022-06-10 10:36:14 +0200

    Añadido

---
+++ main.js
@@ -0,0 +1,13 @@
+
+function encender() { 
+   var carta = document.getElementById('carta');
+      carta.src = "on.jpg";
+   
+} 
+
+
+function apagar() { 
+   var carta = document.getElementById('carta');
+      carta.src = "off.jpg";
+   
+} 
